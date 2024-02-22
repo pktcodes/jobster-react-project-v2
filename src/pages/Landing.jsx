6660dca008951a11a34458e45b267f67d3a2e09f
Praveen Kumar <praveenkumarthipparam@gmail.com>
2024-02-21 23:55:56 -0600

    Landing Page -Structure

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,30 @@
+import logo from '../assets/images/logo.svg';
+import heroImage from '../assets/images/main.svg';
+
 const Landing = () => {
-  return <h1>Landing</h1>;
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt="Jobster Logo" />
+      </nav>
+      <div className="container page">
+        {/* Info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia,
+            quisquam eius mollitia unde rerum iste nisi ad voluptates ducimus
+            nostrum.
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+
+        <img src={heroImage} alt="Hero Image" className="img main-img" />
+      </div>
+    </main>
+  );
 };
 
 export default Landing;
