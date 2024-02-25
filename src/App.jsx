ba203f76fcf14ec9f8f455c1a995d181795001ea
Praveen Kumar <praveenkumarthipparam@gmail.com>
2024-02-24 23:40:37 -0600

    Style Landing Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,8 @@
-import styled from 'styled-components';
 import Landing from './pages/Landing';
-
-const Button = styled.button`
-  background-color: red;
-  color: white;
-  font-size: 2rem;
-`;
-
-const SecondButton = styled.button`
-  background-color: blue;
-  color: white;
-  font-size: 3rem;
-`;
 
 function App() {
   return (
     <>
-      <Button>Click Me</Button>
-      <SecondButton>Click Me</SecondButton>
       <Landing />
     </>
   );
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,9 +1,10 @@
+import styled from 'styled-components';
 import logo from '../assets/images/logo.svg';
 import heroImage from '../assets/images/main.svg';
 
 const Landing = () => {
   return (
-    <main>
+    <Wrapper>
       <nav>
         <img src={logo} alt="Jobster Logo" />
       </nav>
@@ -24,8 +25,46 @@ const Landing = () => {
 
         <img src={heroImage} alt="Hero Image" className="img main-img" />
       </div>
-    </main>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.main`
+  nav {
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display: flex;
+    align-items: center;
+  }
+  .page {
+    min-height: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    margin-top: -3rem;
+  }
+  h1 {
+    font-weight: 700;
+    span {
+      color: var(--primary-500);
+    }
+  }
+  p {
+    color: var(--grey-600);
+  }
+  .main-img {
+    display: none;
+  }
+  @media (min-width: 992px) {
+    .page {
+      grid-template-columns: 1fr 1fr;
+      column-gap: 3rem;
+    }
+    .main-img {
+      display: block;
+    }
+  }
+`;
 
 export default Landing;
