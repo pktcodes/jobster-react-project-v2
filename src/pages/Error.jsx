7469b64f46bc627c32964e3ca5348bee77bf2be0
Praveen Kumar <praveenkumarthipparam@gmail.com>
2024-02-27 22:32:54 -0600

    Error Page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,45 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+import img from '../assets/images/not-found.svg';
+
 const Error = () => {
-  return <h1>Error</h1>;
+  return (
+    <Wrapper className="full-page">
+      <div>
+        <img src={img} alt="not found" />
+        <h3>Ohh! Page not found</h3>
+        <p>{`We can't seem to find the page you're looking for`}</p>
+        <Link to="/">back home</Link>
+      </div>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.main`
+  text-align: center;
+  img {
+    width: 90vw;
+    max-width: 600px;
+    display: block;
+    margin-bottom: 2rem;
+  }
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  h3 {
+    margin-bottom: 0.5rem;
+  }
+  p {
+    margin-top: 0;
+    margin-bottom: 0.5rem;
+    color: var(--grey-500);
+  }
+  a {
+    color: var(--primary-500);
+    text-decoration: underline;
+    text-transform: capitalize;
+  }
+`;
 
 export default Error;
