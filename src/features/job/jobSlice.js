c78d26c1c2d5ff2ca2c000bbeb4fc8f0ed2dd0da
Praveen Kumar <praveenkumarthipparam@gmail.com>
2024-03-23 23:15:05 -0500

    Create JobSlice

---
+++ src/features/job/jobSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  isLoading: false,
+  position: '',
+  company: '',
+  jobLocation: '',
+  status: 'pending',
+  statusOptions: ['interview', 'declined', 'pending'],
+  jobType: 'full-time',
+  jobTypeOptions: ['full-time', 'part-time', 'remote', 'internship'],
+};
+
+const jobSlice = createSlice({
+  name: 'job',
+  initialState: initialState,
+});
+
+export default jobSlice.reducer;
--- src/store.js
+++ src/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import userReducer from './features/user/userSlice';
 import sidebarReducer from './features/sidebar/sidebarSlice';
+import jobReducer from './features/job/jobSlice';
 
 const store = configureStore({
   reducer: {
     userState: userReducer,
     sidebarState: sidebarReducer,
+    jobState: jobReducer,
   },
 });
 
